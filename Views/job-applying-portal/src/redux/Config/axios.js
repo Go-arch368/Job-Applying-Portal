06214f7270125d61afdd83b76167e752bc4f9a29
Gowtham T <gowthamtj0808@gmail.com>
2026-04-18 15:53:10 +0530

    feat: configure axios instance with dynamic base URL from environment variables

--- Views/job-applying-portal/src/redux/Config/axios.js
+++ Views/job-applying-portal/src/redux/Config/axios.js
@@ -2,6 +2,6 @@ import axios from 'axios';
 const apiurl = (import.meta.env.VITE_API_URL || "").replace(/[""]+/g, '');
 export default axios.create({
     // baseURL:"http://localhost:4010"
-    baseURL: apiurl.startsWith("http") ? apiurl : `http://${apiurl}`
+    baseURL: apiurl.startsWith("http") ? apiurl : `https://${apiurl}`
 
 })
