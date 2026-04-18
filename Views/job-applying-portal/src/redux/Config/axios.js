207858fb83f3e3d216528bbba19a4d20efca926f
Gowtham T <gowthamtj0808@gmail.com>
2026-04-18 15:41:50 +0530

    imported axios from frontend

--- Views/job-applying-portal/src/redux/Config/axios.js
+++ Views/job-applying-portal/src/redux/Config/axios.js
@@ -1,4 +1,7 @@
-import axios from 'axios'
+import axios from 'axios';
+const apiurl = (import.meta.env.VITE_API_URL || "").replace(/[""]+/g, '');
 export default axios.create({
-    baseURL:"http://localhost:4010"
+    // baseURL:"http://localhost:4010"
+    baseURL: apiurl.startsWith("http") ? apiurl : `http://${apiurl}`
+
 })
